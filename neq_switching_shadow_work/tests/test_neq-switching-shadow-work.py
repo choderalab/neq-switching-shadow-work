c41e5aaacfea4da279c8967a51a8432de0c6a797
pgrinaway <pgrinaway@gmail.com>
2018-06-20 22:29:23 -0400

    correct module name

---
+++ neq_switching_shadow_work/tests/test_neq-switching-shadow-work.py
@@ -0,0 +1,9 @@
+
+
+import neq_switching_shadow_work
+import pytest
+import sys
+
+def test_neq-switching-shadow-work_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "neq-switching-shadow-work" in sys.modules
